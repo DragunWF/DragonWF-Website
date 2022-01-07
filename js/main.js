49be3ec0041b4110ec0e00936c70dab6bff8be5f
DragonWF <plarisanmarc@gmail.com>
2022-01-07 20:45:17 +0800

    Added content and functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function () {
-    
-})
+  const hiddenContents = [".description-content", ".secondary-content"];
+  for (content of hiddenContents) {
+    $(content).hide();
+  }
+
+  $("#expand").click(() => {
+    $("#expand").slideUp(500, () => {
+      $(hiddenContents[0]).slideDown(500, () => {
+        $(hiddenContents[1]).slideDown(1000);
+      });
+    });
+  });
+
+  const pageUrl = window.location.href;
+  const pageId = pageUrl.substring(pageUrl.lastIndexOf("#") + 1);
+  if (pageId == "latest") {
+    $("html, body").animate(
+      {
+        scrollTop: $(`#${pageId}-project`).offset().top + 150,
+      },
+      850
+    );
+  }
+
+  $("#secret").click(() => {
+    $("#secret").attr("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  });
+});
