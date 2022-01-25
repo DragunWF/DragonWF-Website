097e09f4e4d91df277812a5f037de27ab3e727f5
DragonWF <plarisanmarc@gmail.com>
2022-01-25 22:18:37 +0800

    Added functionality to forms

--- js/main.js
+++ js/main.js
@@ -26,4 +26,25 @@ $(document).ready(function () {
   $("#secret").click(() => {
     $("#secret").attr("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
   });
+
+  // Form
+  $("#form-submit").click(() => {
+    const name = $("#form-name").val();
+    const message = $("#form-textarea").val();
+    if (message) {
+      if (!name) $("#form-name").val("Anonymous");
+      if (name.length < 20 && message.length < 254)
+        $("#form-submit").attr("type", "submit");
+      else {
+        if (name.length > 20) alert("Name must be less than 20 characters");
+        if (message.length > 254)
+          alert("Message must be less than 254 characters");
+      }
+    } else alert("Your message is empty!");
+  });
+
+  $("#form-reset").click(() => {
+    $("#form-name").val("");
+    $("#form-textarea").val("");
+  });
 });
